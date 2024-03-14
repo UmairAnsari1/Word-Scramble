0ab809cec1f584464c5f702c4c2ab2a075c89499
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-03-14 18:03:25 +0530

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Umair on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
